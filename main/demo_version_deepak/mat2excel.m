5d7893430e3828f7b92efd3c3503fb51919597d9
deepakksingh <be.cse.deepak@gmail.com>
2019-04-04 21:35:51 +0530

    added project report and presentation and made directory hierarchy changes

--- main/demo_version_deepak/mat2excel.m
+++
@@ -1,5 +0,0 @@
- data=load('./cache/forest/model.mat');
- f=fieldnames(data);
- for k=1:size(f,1)
-   xlswrite('model.xlsx',data.(f{k}),f{k})
- end
--- main/demo_version_deepak/private/mexomp.m
+++
@@ -1,7 +0,0 @@
-function mexomp( cppFile )
-% mexomp( cppFile )
-% Compile mex file with OpenMP opts set
-
-cmd = ['mex %s ''-DUSEOMP'' CXXFLAGS="\\$CXXFLAGS ' ...
-       '-fopenmp" LDFLAGS="\\$LDFLAGS -fopenmp"'];
-eval( sprintf(cmd,cppFile) );
